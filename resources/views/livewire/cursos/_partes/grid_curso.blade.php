2c8a94be04751d3ac1a1d14f3e89dd15cf994e07
telmoDev <83423970+telmoDev@users.noreply.github.com>
2023-01-23 21:56:15 -0500

    last change v2

--- resources/views/livewire/cursos/_partes/grid_curso.blade.php
+++ resources/views/livewire/cursos/_partes/grid_curso.blade.php
@@ -2,7 +2,7 @@
     x-data="{ tooltip: false }"
     class="box-curso tema text-center mb-4  ">
         <div class="relative">
-        <a href="{{ $curso->url() }}" class="box-imagen block w-full h-64 bg-center bg-cover transition-all" style="background-image: url({{ $curso->imagen() }})"></a>
+        <a href="{{ $curso->url() }}" class="box-imagen block w-full h-64 bg-center bg-cover transition-all" style="background-image: url({{ $curso->getImagen() }})"></a>
         <div class="text-left principal ">
             <h3 class="text-lg font-bold px-2 transition-all"><a href="{{ $curso->url() }}" class="block">{{ $curso->nombre}}</a></h3>
             <div class="price text-sm font-bold px-2">$. {{ $curso->precio }}</div>
--- resources/views/livewire/cursos/_partes/grid_curso_mis_cursos.blade.php
+++ resources/views/livewire/cursos/_partes/grid_curso_mis_cursos.blade.php
@@ -3,7 +3,7 @@
     class="box-curso tema text-center mb-4  ">
         <div class="relative">
 
-            <a href="{{ $curso->url() }}" class="box-imagen block w-full h-64 bg-center bg-cover transition-all" style="background-image: url({{ $curso->imagen() }})"></a>
+            <a href="{{ $curso->url() }}" class="box-imagen block w-full h-64 bg-center bg-cover transition-all" style="background-image: url({{ $curso->getImagen() }})"></a>
             <div class="text-left principal ">
                 <h3 class="title  text-lg font-bold px-2 transition-all"><a href="{{ $curso->url() }}" class="block">{{ $curso->nombre}}</a></h3>
 
