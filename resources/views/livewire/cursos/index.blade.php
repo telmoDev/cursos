a855fe2a6d4789dbcd61c7e18992389e77824de9
CarlosTontaquimba <carlos.tontaquimba@educacion.gob.ec>
2023-06-19 23:37:42 -0500

    Profesores cards añadido

--- resources/views/livewire/cursos/index.blade.php
+++ resources/views/livewire/cursos/index.blade.php
@@ -69,7 +69,45 @@
                             <div class="container mx-auto px-4 sm:px-6 lg:px-8">
                                 <div class="flex flex-col items-center">
                                     <h1 class="text-2xl font-bold text-gray-800 mb-6">¿Con qui&eacute;n vas a aprender?</h1>
+                                    <div class="flex justify-center">
+                                        <div class="w-1/3 p-4">
+                                            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                                <div class="flex justify-end px-4 pt-4">
 
+                                                </div>
+                                                <div class="flex flex-col items-center pb-10">
+                                                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset('/img/el_paso.jpg') }}" alt="Bonnie image" />
+                                                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                                                    <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="w-1/3 p-4">
+                                            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                                <div class="flex justify-end px-4 pt-4">
+
+                                                </div>
+                                                <div class="flex flex-col items-center pb-10">
+                                                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset('/img/el_paso.jpg') }}" alt="Bonnie image" />
+                                                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                                                    <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="w-1/3 p-4">
+                                            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                                <div class="flex justify-end px-4 pt-4">
+
+                                                </div>
+                                                <div class="flex flex-col items-center pb-10">
+                                                    <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="{{ asset('/img/el_paso.jpg') }}" alt="Bonnie image" />
+                                                    <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Bonnie Green</h5>
+                                                    <span class="text-sm text-gray-500 dark:text-gray-400">Visual Designer</span>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
